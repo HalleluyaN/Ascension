652756afa50eb754d84bc007092a44449b08aebe
HalleluyaN <halleluya.mengesha@gmail.com>
2023-05-23 20:11:43 -0500

    Thirteenth Commit

--- ascension1/src/test/java/test/com/ascension/pageobject/PriceTransparencyPOTest.java
+++ ascension1/src/test/java/test/com/ascension/pageobject/PriceTransparencyPOTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
@@ -32,11 +33,14 @@ public class PriceTransparencyPOTest {
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-//        WebDriverManager.getInstance(SafariDriver.class).setup();
+        //        WebDriverManager.getInstance(SafariDriver.class).setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1400,800");
         WebDriverManager.chromedriver().setup();
 //        driver = new SafariDriver();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
